ba2cb2bb538728cbb7fed66665559a7c9b00703c
xiao106 <1355295243@qq.com>
2020-09-05 09:43:07 +0800

    这是新的小案例增删改查2-2

--- index.js
+++ index.js
@@ -122,38 +122,92 @@
             const h = snabbdom.h;
             const box = document.querySelector(".box");
             const tab = document.querySelector(".tab");
+            const search = document.querySelector(".search");
             const data = {
-                lists: [1, 2, 3]
+                lists: ['1', '2', '3'],
+                flag: false,
+                changeVal: ''
             };
 
-            //!渲染一个input
+            //!渲染一个添加input
             function renderInput() {
-                patch(tab, h('input', {
-                    props: {
-                        type: "text",
-                        placeholder: "请输入内容",
-                        onkeydown: e => {
-                            addLists(e)
+                patch(tab, h("div", {}, [
+                    h('input', {
+                        props: {
+                            type: "text",
+                            placeholder: "请输入内容",
+                            onkeydown: e => {
+                                addLists(e)
+                            }
                         }
-                    }
-                }))
+                    }),
+                    h("button", {
+                        props: {
+                            onclick: e => {
+                                clickaddLists(e)
+                            }
+                        }
+                    }, "添加")
+                ]))
             };
-            //!添加渲染项,为data.lists添加一项内容
-            function addLists(e) {
-                if (e.keyCode == 13) {
-                    const box = getBox();
-                    data.lists.push(e.target.value);
-                    patch(box, renderLists(data.lists));
-                    e.target.value = "";
-                }
-            };
-            //!删除一项
-            function delLists() {
-
-            }
-
-
             renderInput();
+            //!渲染一个查询input
+            function searchInput() {
+                patch(search, h("div", {
+                    props: {
+                        className: "search"
+                    }
+                }, [
+                    h('input', {
+                        props: {
+                            type: "text",
+                            placeholder: "请输入内容",
+                        }
+                    }),
+                    h("button", {
+                        props: {
+                            onclick: e => {
+                                searchLists(e)
+                            }
+                        }
+                    }, "查询")
+                ]))
+            };
+
+            //!渲染一个修改的input
+            function changeInput(change) {
+                patch(change, h("div", {
+                    props: {
+                        className: "change"
+                    },
+                    style: {
+                        display: data.flag ? 'block' : 'none'
+                    }
+                }, [
+                    h("input", {
+                        props: {
+                            type: "text",
+                            value: data.changeVal
+                        }
+                    }),
+                    h("button", {
+                        props: {
+                            onclick: () => {
+                                flagHide();
+                            },
+                        }
+                    }, "取消"),
+                    h("button", {
+                        props: {
+                            onclick: (e) => {
+                                changeLists(e)
+                            },
+                        }
+                    }, "确定"),
+
+                ]))
+            }
+            searchInput();
             //!渲染列表vdom
             function renderLists(arr) {
                 return h("ul", {
@@ -169,7 +223,9 @@
                         h("p", {}, item),
                         h("button", {
                             props: {
-                                onclick: () => {},
+                                onclick: (e) => {
+                                    flagShow(e)
+                                },
                             }
                         }, "修改"),
                         h("button", {
@@ -182,7 +238,67 @@
                     ])
                 }))
             }
+
             patch(box, renderLists(data.lists));
+
+
+            //!keydow事件添加渲染项,为data.lists添加一项内容
+            function addLists(e) {
+                if (e.keyCode == 13) {
+                    const box = getBox();
+                    data.lists.push(e.target.value);
+                    patch(box, renderLists(data.lists));
+                    e.target.value = "";
+                }
+            };
+            //!鼠标点击添加渲染项,为data.lists添加一项内容
+            function clickaddLists(e) {
+                const box = getBox();
+                data.lists.push(e.target.previousElementSibling.value);
+                patch(box, renderLists(data.lists));
+                e.target.previousElementSibling.value = "";
+
+            };
+            //!删除一项
+            function delLists(index) {
+                const box = getBox();
+                data.lists.splice(index, 1);
+                patch(box, renderLists(data.lists));
+            }
+            //!查询某一项
+            function searchLists(e) {
+                const box = getBox();
+                const result = data.lists.filter(item => {
+                    return item == e.target.previousElementSibling.value
+                });
+                patch(box, renderLists(result));
+                e.target.previousElementSibling.value = "";
+            }
+            //修改某一项
+            function changeLists(e) {
+                const box = getBox();
+                let val = e.target.previousElementSibling.previousElementSibling.value;
+                let index = data.lists.indexOf(data.changeVal);
+                data.lists.splice(index, 1, val)
+                patch(box, renderLists(data.lists));
+                e.target.previousElementSibling.previousElementSibling.value = '';
+            }
+            //修改列表的显示--隐藏
+            function flagShow(e) {
+                data.flag = true;
+                data.changeVal = e.target.previousElementSibling.innerHTML;
+                const change = document.querySelector(".change");
+                changeInput(change);
+
+            }
+
+            function flagHide() {
+                data.flag = false;
+                const change = document.querySelector(".change");
+                changeInput(change);
+            }
+
+
             //! 获取元素
             function getBox() {
                 return document.querySelector(".box");
